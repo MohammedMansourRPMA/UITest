d97db611de3c0e8fff579817b323f98e2f90a4ff
MohammedMansourNseer <mohammedmansournseer@gmail.com>
2021-11-14 13:12:24 +0200

    firstPush

---
+++ app/src/main/java/com/rpmnetworks/uitset/ShowTextActivity.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rpmnetworks.uitset;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.common.base.Strings;
+
+
+public class ShowTextActivity extends Activity {
+
+    // The name of the extra data sent through an {@link Intent}.
+    public final static String KEY_EXTRA_MESSAGE =
+            "com.example.android.testing.espresso.basicsample.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_text);
+
+        // Get the message from the Intent.
+        Intent intent = getIntent();
+        String message = Strings.nullToEmpty(intent.getStringExtra(KEY_EXTRA_MESSAGE));
+
+        // Show message.
+        ((TextView)findViewById(R.id.show_text_view)).setText(message);
+    }
+
+    static protected Intent newStartIntent(Context context, String message) {
+        Intent newIntent = new Intent(context, ShowTextActivity.class);
+        newIntent.putExtra(KEY_EXTRA_MESSAGE, message);
+        return newIntent;
+    }
+}
